5ac0ff8f8c9f58957a6deb6e747aed50d404a34a
Leah Griffin <griffin.nate9@gmail.com>
2019-04-06 23:55:46 +0000

    payments with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,26 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
   def show
   end
     
   private
 
   helper_method :current_lesson
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      redirect_to course_path(current_lesson.section.course), alert: "You must enroll in the course to view this page"
+    end
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end 
- end
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle, :video)
+  end
+end
+
